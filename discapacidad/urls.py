f1587593aa59127ed3a15738cbd385f376d3a442
veronny393 <veronny393@hotmail.com>
2024-05-13 17:35:21 -0500

    actualiza establecimientos  reporte operacional

--- discapacidad/urls.py
+++ discapacidad/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-from .views import RptOperacinalProv, RptOperacinalDist, RptOperacinalRed 
+from .views import RptOperacinalProv, RptOperacinalDist, RptOperacinalRed, RptOperacinalMicroRed, RptOperacinalEstablec
 
 urlpatterns = [
     #discapacidad
@@ -25,12 +25,17 @@ urlpatterns = [
     #microredes
     path('get_microredes/<int:microredes_id>/', views.get_microredes, name='get_microredes'),
     path('p_microredes/', views.p_microredes, name='p_microredes'),
+    #-- microredes excel
+    path('rpt_operacional_microred_excel/', RptOperacinalMicroRed.as_view(), name = 'rpt_operacional_microred_xls'),
     
     # establecimientos
     path('get_establecimientos/<int:establecimiento_id>/', views.get_establecimientos, name='get_establecimientos'),
     path('p_microredes_establec/', views.p_microredes_establec, name='p_microredes_establec'),
     path('p_establecimiento/', views.p_establecimientos, name='p_establecimientos'),    
     
+    #-- estableccimiento excel
+    path('rpt_operacional_establec_excel/', RptOperacinalEstablec.as_view(), name = 'rpt_operacional_establec_xls'),
+     
    #path('establecimiento/', views.establecimiento, name='establecimiento'),
    #path('obtener_microredes/', views.obtener_microredes, name='obtener_microredes'),
    #path('obtener_establecimientos/', views.obtener_establecimientos, name='obtener_establecimientos'),
